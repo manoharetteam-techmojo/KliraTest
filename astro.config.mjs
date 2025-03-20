8a774c9d1aa3825d432b1ae87285dd494c2dba82
manoharetteam-techmojo <manohar.etteam@techmojo.in>
2025-03-20 18:49:14 +0530

    added server configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+});
